abf599480014d0e1b43e91efa322beb533d02ea9
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2025-01-02 17:58:41 +0530

    set up a dialog for shoe each recipe data

---
+++ src/components/recipe/saved-recipe.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import axiosInstance from "@/utils/axios";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { MdDelete } from "react-icons/md";
+import Link from "next/link";
+
+interface SavedRecipe {
+  _id: string;
+  title: string;
+  image: string;
+  description: string;
+}
+
+export default function SavedRecipe() {
+  const [selectedRecipe, setSelectedRecipe] = useState<SavedRecipe | null>(
+    null
+  );
+
+  const fetchSavedRecipe = async () => {
+    const response = await axiosInstance.get(`/recipe/get-saved-recipes`);
+    if (response.status !== 200) {
+      throw new Error("Something went wrong");
+    }
+    return response.data;
+  };
+
+  const { data } = useQuery<{ data: SavedRecipe[] }, Error>({
+    queryKey: ["fetchSavedRecipe"],
+    queryFn: fetchSavedRecipe,
+  });
+
+  return (
+    <div className="flex flex-col items-center p-5">
+      <h1 className="text-5xl font-extrabold font-serif">My Saved Recipes</h1>
+      <div>
+        <h1 className="text-xl font-extrabold mt-4">Recently Saved</h1>
+
+        <div className="flex flex-col items-center mt-4">
+          {data?.data.length === 0 ? (
+            <p className="text-center text-gray-500">
+              There are no saved recipes.
+            </p>
+          ) : (
+            <>
+              <div className="flex gap-2 flex-wrap justify-center">
+                {data?.data.map((recipe) => (
+                  <div
+                    key={recipe._id}
+                    className="max-w-[170px] border cursor-pointer"
+                    onClick={() => setSelectedRecipe(recipe)}
+                  >
+                    <Image
+                      src={
+                        recipe.image ||
+                        "http://localhost:3000/_next/image?url=https%3A%2F%2Fwww.allrecipes.com%2Fimg%2Ficons%2Frecipe-add-photo-square.jpg&w=384&q=75"
+                      }
+                      alt={recipe.title || "Recipe Image"}
+                      width={170}
+                      height={170}
+                      className="object-cover w-[150px] h-[150px]"
+                    />
+                    <h3 className="font-bold text-sm p-1">{recipe.title}</h3>
+                  </div>
+                ))}
+              </div>
+
+              {selectedRecipe && (
+                <Dialog
+                  open={!!selectedRecipe}
+                  onOpenChange={() => setSelectedRecipe(null)}
+                >
+                  <DialogContent className="p-0 border-none">
+                    <DialogTitle className="hidden"></DialogTitle>
+                    <DialogHeader className="p-0">
+                      <div className="flex gap-6">
+                        <div className="w-1/2">
+                          <Image
+                            src={selectedRecipe.image || "/images/loginImg.png"}
+                            alt={selectedRecipe.title || "Recipe Image"}
+                            width={400}
+                            height={400}
+                            className="h-full w-full object-cover"
+                          />
+                        </div>
+                        <div className="w-1/2 flex flex-col justify-between gap-10 pt-7 p-4 pb-7">
+                          <div className="flex flex-col gap-4">
+                            <h1 className="text-xl font-extrabold">
+                              {selectedRecipe.title}
+                            </h1>
+
+                            <p className="text-sm text-gray-700 mt-2">
+                              {selectedRecipe.description}
+                            </p>
+                            <Link href={`/recipe/${selectedRecipe._id}`}>
+                              {" "}
+                              <Button
+                                variant="ghost"
+                                className="w-full border-none p-3 uppercase bg-customColor text-md font-bold text-white rounded-none"
+                              >
+                                View Recipe
+                              </Button>
+                            </Link>
+                          </div>
+                          <div className="flex items-center gap-1 cursor-pointer hover:underline">
+                            <MdDelete />
+                            <a href="#" className="text-xs">
+                              Remove from Saved Recipes
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </DialogHeader>
+                  </DialogContent>
+                </Dialog>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
